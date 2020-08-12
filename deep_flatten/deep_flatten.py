2349734b79151a8e24f26ed257b2899772a1c9a1
João Palmeiro <joaommpalmeiro@gmail.com>
2020-08-12 15:58:16 +0100

    Updated: deep_flatten.

--- deep_flatten/deep_flatten.py
+++ deep_flatten/deep_flatten.py
@@ -4,7 +4,17 @@ from typing import Iterator
 
 def deep_flatten(lst: Iterable) -> Iterator[Iterable]:
     for el in lst:
-        if isinstance(el, Iterable) and not isinstance(el, str):
+        if isinstance(el, Iterable) and not isinstance(el, (str, bytes)):
             yield from deep_flatten(el)
         else:
             yield el
+
+
+# print(list(deep_flatten([[(1, 2), (3, 4)], [(5, 6), (7, 8)]])))
+# print(list(deep_flatten([["apple", "pickle"], ["pear", "avocado"]])))
+
+# numbers_and_words = enumerate([99, 98, 97])
+# flattened = deep_flatten(numbers_and_words)
+# print(next(numbers_and_words))
+# print(next(flattened))
+# print(next(flattened))
